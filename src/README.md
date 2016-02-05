7cf8eb8538047de1f62f03e9dd66814ce190b5a3
tmosbruger <tmosbruger@yahoo.com>
2016-02-05 15:49:41 -0700

    Initial Commit via Eclipse

---
+++ src/README.md
@@ -0,0 +1,32 @@
+# README #
+
+**Code does not include gnomex_httpclient.properties, which is necessary to interact with the GNomEx DB. I have this if you need it. It contains admin
+login info for creating new experiment-linked analysis reports.**
+
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+### What is this repository for? ###
+
+* Quick summary
+* Version
+* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+Contact Darren Ames (darren.ames@hci.utah.edu) with any questions or comments. 
---
+++ src/autoaligner/StandardDeviation.java
@@ -0,0 +1,65 @@
+package autoaligner;
+
+
+import java.io.Serializable;
+
+/**Uses running totals to calculate the variance and standard deviation of an array of numbers.*/
+public class StandardDeviation implements Serializable {
+	
+	//running fields
+	double numberObservations = 0;
+	double sum = 0;
+	double sumSquares = 0;
+	double mean = Double.MIN_NORMAL;
+	double standardDeviation = Double.MIN_NORMAL;
+	private static final long serialVersionUID = 1L;
+	
+	/**Count each number as it becomes available.*/
+	public void count(double number){
+		numberObservations++;
+		sum+= number;
+		sumSquares+= (number*number);
+		//watch out for negative values indicating that they have exceeded the size 
+		if (sumSquares < 0) System.err.println("You have exceeded the size of the StandardDeviation counters with value "+number);
+	}
+
+	public double getMean() {
+		if (mean == Double.MIN_NORMAL) mean = sum/numberObservations;
+		return mean;
+	}
+
+	public double getVariance() {
+		return (sumSquares - sum * getMean()) / (numberObservations - 1.0);
+	}
+
+	public double getStandardDeviation() {
+		if (standardDeviation == Double.MIN_NORMAL) standardDeviation = Math.sqrt(getVariance());
+		return standardDeviation;
+	}
+	
+	/**@return mean tab standard deviation*/
+	public String toString(){
+		return getMean()+"\t"+getStandardDeviation();
+	}
+
+	public double getNumberObservations() {
+		return numberObservations;
+	}
+	
+	public double getCoefficientOfVariation(){
+		return getStandardDeviation()/ getMean();
+	}
+	
+	public double getSignal2Noise(){
+		return getMean()/ getStandardDeviation();
+	}
+	/**returns (Score - Mean) / Standard Deviation*/
+	public double getZScore(double score){
+		if (mean == Double.MIN_NORMAL){
+			getMean();
+			getStandardDeviation();
+		}
+		return (score - mean) / standardDeviation;
+	}
+	
+}
